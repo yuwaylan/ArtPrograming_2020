7f45ac721f31d98e4e2bbdf5c9a39a734954e818
yuwaylan <0987431104a@gmail.com>
2021-01-06 22:48:02 +0800

    add final proj

---
+++ final_proj/Art_programing_final_proj/Art_programing_final_proj/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Art_programing_final_proj
+//
+//  Created by ChengHanYU on 2021/1/6.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
